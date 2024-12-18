f6df3678c9478da61cd21503fe759425898c4c90
Le Quy Do <dolq@gmail.com>
2024-12-18 10:08:01 +0700

    change url

--- src/services/mediaServices.ts
+++ src/services/mediaServices.ts
@@ -132,9 +132,7 @@ class MediasService {
         return {
           url: isProduction
             ? `${env.host}/medias/video-hls/${fileUploaded.newFilename.split('.')[0]}/master.m3u8`
-            : `http://192.168.237.103:${env.port}/medias/video-hls/${
-                fileUploaded.newFilename.split('.')[0]
-              }/master.m3u8`,
+            : `http://localhost:${env.port}/medias/video-hls/${fileUploaded.newFilename.split('.')[0]}/master.m3u8`,
           type: MediaType.VideoHLS
         };
       })

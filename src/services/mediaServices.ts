036f264714f3170e6267a083ade9778551dc7fb4
Le Quy Do <dolq@gmail.com>
2025-01-02 09:54:21 +0700

    update

--- src/services/mediaServices.ts
+++ src/services/mediaServices.ts
@@ -130,9 +130,9 @@ class MediasService {
       filesUploaded.map(async (fileUploaded) => {
         encodeVideoQueue.enqueue(fileUploaded.filepath);
         return {
-          url: `https://masterlearning.leedowork.id.vn:${env.port}/medias/video-hls/${
-            fileUploaded.newFilename.split('.')[0]
-          }/master.m3u8`,
+          url: isProduction
+            ? `${env.host}/medias/video-hls/${fileUploaded.newFilename.split('.')[0]}/master.m3u8`
+            : `http://localhost:${env.port}/medias/video-hls/${fileUploaded.newFilename.split('.')[0]}/master.m3u8`,
           type: MediaType.VideoHLS
         };
       })
